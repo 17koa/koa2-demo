4f6b42549ccd657d2c5bbfd1a22b774ff9188c00
i5ting <i5ting@126.com>
2016-04-10 16:52:40 +0800

    refact

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # koa2-demo
+
+## Getting Start
+
+```
+git clone https://github.com/17koa/koa2-demo.git
+cd koa2-demo
+npm install
+npm start
+```
+
+open in browser
+
+http://127.0.0.1:3000/ 
+
+## 支持koa2
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var router = require('koa-router')();
 
 router.get('/', function (ctx, next) {
   ctx.render('index', {
-    title: 'Hello World Koa!'
+    title: 'Hello World Koa 2!'
   });
 })
 module.exports = router;
